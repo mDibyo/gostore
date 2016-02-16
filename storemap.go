aca80ee74377ab7382e0730c54f6125866d901f6
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2016-02-16 00:17:26 -0800

    Add logManager shell

--- storemap.go
+++ storemap.go
@@ -1,9 +1,36 @@
+/*
+Package gostore implements a simple single-node log-based key-value
+store. It supports multiple concurrent transactions through a set of
+locks on values.
+*/
 package gostore
 
 import (
+	pb "github.com/mDibyo/gostore/pb"
 	"sync"
 )
 
+// Key represents a key in the store
+type Key string
+
+// Value represents the value for a key in the key store
+type Value []byte
+
+type StoreMapValue struct {
+	value Value
+	lock  sync.RWMutex
+}
+
+type logManager struct {
+	log      pb.Log
+	storeMap map[Key]StoreMapValue
+}
+
+var lm logManager
+
+var currentTid int64 = 0
+
+// TransactionID is used to uniquely identify/represent a transaction
 type TransactionID int64
 
 // New Transaction creates a new transaction and returns its TransactionID.
@@ -12,7 +39,7 @@ func NewTransaction() (tid TransactionID) {
 }
 
 // Commit commits and ends the transaction with this TransactionID.
-func (tid TransactionID) Commit() {
+func (tid TransactionID) Commit() (err error) {
 	return
 }
 
@@ -22,50 +49,54 @@ func (tid TransactionID) Abort() {
 }
 
 // Get retrieves the value of a key in transaction with this TransactionID.
-func (tid TransactionID) Get(key string) (value []byte, err error) {
+func (tid TransactionID) Get(key Key) (value Value, err error) {
 	return
 }
 
 // Set sets the value of a key in transaction with this TransactionID.
-func (tid TransactionID) Set(key string, value []byte) (err error) {
+func (tid TransactionID) Set(key Key, value Value) (err error) {
 	return
 }
 
 // Delete deletes a key in transaction with this TransactionID.
-func (tid TransactionID) Delete(key string) (err error) {
+func (tid TransactionID) Delete(key Key) (err error) {
 	return
 }
 
 // Get retrieves the value of a key in a new single-operation transaction.
-func Get(key string) (value []byte, err error) {
+func Get(key Key) (value Value, err error) {
 	tid := NewTransaction()
 	value, err = tid.Get(key)
 	if err != nil {
 		tid.Abort()
 		return
 	}
-	tid.Commit()
+	err = tid.Commit()
 	return
 }
 
 // Set sets the value of a key in a new single-operation transaction.
-func Set(key string, value []byte) (err error) {
+func Set(key Key, value Value) (err error) {
 	tid := NewTransaction()
 	if err = tid.Set(key, value); err != nil {
 		tid.Abort()
 		return
 	}
-	tid.Commit()
+	err = tid.Commit()
 	return
 }
 
 // Delete deletes a key in a new single-operation transaction.
-func Delete(key string) (err error) {
+func Delete(key Key) (err error) {
 	tid := NewTransaction()
 	if err = tid.Delete(key); err != nil {
 		tid.Abort()
 		return
 	}
-	tid.Commit()
+	err = tid.Commit()
 	return
+}
+
+func init() {
+	lm = logManager{pb.Log{}, make(map[Key]StoreMapValue)}
 }
